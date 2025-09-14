7819f8d174db5e1c97ee9bfbf7af8149a44365a3
Wenxuan Jiang <wxjiang.stats@gmail.com>
2025-09-13 21:25:45 -0400

    change headshot and favicon

--- generate_Quarto_website_favicon.R
+++ generate_Quarto_website_favicon.R
@@ -1,15 +1,15 @@
 require(RColorBrewer)
 
-set.seed(5)
+set.seed(1)
 
 d = runif(9)
 d_matrix = matrix(d, ncol = 3)
 
-# png(filename = "./heatmap_favicon.png", res = 300)
+png(filename = "./images/heatmap_favicon.png", width = 5, height = 5, units = "in", res = 300)
 
 heatmap(d_matrix, Colv = NA, Rowv = NA, 
         labRow = NA, labCol = NA, 
         scale="column",
-        col= colorRampPalette(brewer.pal(8, "Blues"))(25))
+        col= colorRampPalette(brewer.pal(8, "Blues"))(9))
 
-# dev.off()
+dev.off()
